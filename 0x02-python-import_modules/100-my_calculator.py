c05ed638318b8008baed4e82535c640ac17e227a
Olayemi Peter Pelumi <peterpelumi9@gmail.com>
2022-12-02 05:23:43 +0100

    new

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-from sys import argv
-
 if __name__ == "__main__":
-    if len(argv) != 4:
-        print("Usage:", argv[0], "<a> <operator> <b>")
-        exit(1)
-
-    op = argv[2]
-    f = {"+": add, "-": sub, "*": mul, "/": div}
-    if op not in f:
+    import sys
+    from calculator_1 import add, sub, mul, div
+    args = len(sys.argv)
+    if(args != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    operator = sys.argv[2]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if(operator in "+-*/"):
+        if operator is "+":
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        elif operator is "-":
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+        elif operator is "*":
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+        else:
+            print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-
-    a = int(argv[1])
-    b = int(argv[3])
-    print("{:d} {:s} {:d} = {:d}".format(a, op, b, f[op](a, b)))
+        sys.exit(1)
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    userin = argv[1:]
-    size = len(userin)
-    print("{:d} {:s}{:s}".
-          format(size,
-                 "arguments" if (size) is not 1 else "argument",
-                 "." if (size) is 0 else ":"))
-    for idx, arg in enumerate(userin):
-        print("{:d}: {:s}".format(idx + 1, arg))
+    import sys
+    args = len(sys.argv)
+    if(args <= 1):
+        print("{} arguments.".format(args - 1))
+    elif(args == 2):
+        print("{} argument:".format(args - 1))
+    else:
+        print("{} arguments:".format(args - 1))
+    for x in range(1, args):
+        print("{}: {}".format(x, sys.argv[x]))
